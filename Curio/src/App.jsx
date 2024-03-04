7565390c1f5559e696a08ff064d2de40937faef4
omar-adel1 <108310359+omar-adel1@users.noreply.github.com>
2024-03-04 15:23:10 +0200

    Curio

--- Curio/src/App.jsx
+++ Curio/src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
-// import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Error from './Components/Error/Error.jsx'
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <div>
+      <Routes>
+        <Route path='*' element={<Error/>} />
+      </Routes>
+    </div>
   )
 }
 
