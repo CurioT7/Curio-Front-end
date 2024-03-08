46d7c0978faa6633061322e05c519dbe95ac75b4
omar-adel1 <108310359+omar-adel1@users.noreply.github.com>
2024-03-08 19:22:25 +0200

    ProfileSetting

--- Curio/src/App.jsx
+++ Curio/src/App.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Home from "./Pages/Home/Home.jsx"
+import Navbar from './Components/Navbar/Navbar.jsx'
 import Error from './Components/Error/Error.jsx'
+import Profile_Setting from "./Components/Profile_Setting/Profile_Setting.jsx"
+
+
 
 function App() {
 
 
   return (
     <div>
+      <Navbar/>
+      <Profile_Setting/>
       <Routes>
+        <Route path="/" element={<Home/>} />
         <Route path='*' element={<Error/>} />
       </Routes>
     </div>
