216c98843f00b0b139929992c55ec52bca788c81
Hana Fares <hanafares25@gmail.com>
2024-03-18 00:24:31 +0200

    Component Routing

--- Curio/src/Components/ForgotUser/ForgotUser.jsx
+++ Curio/src/Components/ForgotUser/ForgotUser.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import '../Login/Login.css';
 
-function ForgotUser() {
+
+function ForgotUser({setForgotUser, ForgotUser}) {
   return (
     <>
+    <button className='backButton' onClick={() => setForgotUser(false)} >
+      <svg rpl="" fill="currentColor" height="20" icon-name="back-outline" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19 9.375H2.51l7.932-7.933-.884-.884-9 9a.625.625 0 0 0 0 .884l9 9 .884-.884-7.933-7.933H19v-1.25Z"></path>
+    </svg>
+    </button>
+
       <div className="loginBox">
         <h1>Recover your username</h1>
         <p>
@@ -20,7 +27,7 @@ function ForgotUser() {
               Get Help
             </a>
           </p>
-          <a href="#">Sign up</a> <a href="Login">Log In</a>
+          <a href="#" >Sign up •</a> <a href="Login">Log In</a>
         </form>
       </div>
       <div className="submit">
--- Curio/src/Components/Login/Login.jsx
+++ Curio/src/Components/Login/Login.jsx
@@ -1,101 +1,80 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
+import React from "react";
 import './Login.css';
-import '../ForgotUser/ForgotUser.jsx';
-import ForgotUser from '../ForgotUser/ForgotUser.jsx';
-import ForgotPass from '../ForgotPass/ForgotPass.jsx';
+import './Login.jsx';
 import Google from '../../styles/icons/Google.jsx';
-import CloseButton from '../../styles/icons/CloseButton.jsx';
-import ModalBody from 'react-bootstrap/esm/ModalBody.js';
 
-
-
-Modal.setAppElement('#root');
-
-function Login() {
-  const [forgotUser, setForgotUser] = useState(false);
-  const [forgotPass, setForgotPass] = useState(false);
-  const [isOpen, setIsOpen] = useState(true);
+function Login({ forgotUser, setForgotUser, forgotPass, setForgotPass }){
+  
 
   const handleClose = () => {
     setIsOpen= false;
   };
+    return (
+      <div className="modalParent">
+      <div className="loginBox">
+        <form action="">
+          <h1>Log In</h1>
+          <p>
+            By continuing, you agree to our{' '}
+            <a href="https://www.redditinc.com/policies/user-agreement">
+              User Agreement
+            </a>{' '}
+            and <br />
+            acknowledge that you understand the{' '}
+            <a href="https://www.reddit.com/policies/privacy-policy">
+              {' '}
+              Privacy Policy{' '}
+            </a>
+          </p>
 
-  return (
-    <div>
-      <Modal className="loginModal" isOpen={true} contentLabel="Login Modal"  >
-      {/* <Modal.Header className='border-0'>
-      <div>
-        <button onClick={handleClose}>  <CloseButton /> </button>  
-      </div>     
-        </Modal.Header> */}
+          <div >
+    <button className='continue-with-google p-2 d-flex justify-content-between align-items-center align-content-center' >
+       <div className="logintext">
+       Continue with Google
 
-        <div className="modalParent">
-          {!forgotUser && !forgotPass && (
-            <div className="modalParent">
-              <div className="loginBox">
-                <form action="">
-                  <h1>Log In</h1>
-                  <p>
-                    By continuing, you agree to our{' '}
-                    <a href="https://www.redditinc.com/policies/user-agreement">
-                      User Agreement
-                    </a>{' '}
-                    and <br />
-                    acknowledge that you understand the{' '}
-                    <a href="https://www.reddit.com/policies/privacy-policy">
-                      {' '}
-                      Privacy Policy{' '}
-                    </a>
-                  </p>
-
-                  <div >
-            <button className='continue-with-google p-3 d-flex justify-content-between align-items-center align-content-center' >
-                Continue with Google
-                <div className="loginGoogle">
-                <Google />
-                </div>
-            </button>
+       </div>
+        <div className="loginGoogle">
+        <Google />
         </div>
+    </button>
+</div>
 
-                  <div className="loginInput">
-                    <input type="text" placeholder="Username *" required />
-                  </div>
-                  <div className="loginInput">
-                    <input type="password" placeholder="Password *" required />
-                  </div>
-                  <div className="forgot">
-                    <b>Forgot your </b>
-                    <a href="#" onClick={() => setForgotUser(true)}>
-                      username
-                    </a>{' '}
-                    <b>or</b>{' '}
-                    <a href="#" onClick={() => setForgotPass(true)}>
-                      password
-                    </a>
-                    <b>?</b>
-                  </div>
+<h6 className='or'>
+  <hr />
+     OR  
+    <hr />
+</h6>            
+          <div className="loginInput">
+            <input type="text" placeholder="Username *" required />
+          </div>
+          <div className="loginInput">
+            <input type="password" placeholder="Password *" required />
+          </div>
+          <div className="forgot">
+            <b>Forgot your </b>
+            <a href="#" onClick={() => setForgotUser(true)}>
+              username
+            </a>{' '}
+            <b>or</b>{' '}
+            <a href="#" onClick={() => setForgotPass(true)}>
+              password
+            </a>
+            <b>?</b>
+          </div>
 
-                  <div className="sign-up">
-                    <b>New to Reddit? </b>
-                    <a href="#">Sign up</a>
-                  </div>
-                </form>
-              </div>
-              <div className="submit">
-                <button type="submit" className="login_buttons">
-                  Login
-                </button>
-              </div>
-            </div>
-          )}
-
-          {forgotUser && <ForgotUser />}
-          {forgotPass && <ForgotPass />}
-        </div>
-      </Modal>
+          <div className="sign-up">
+            <b>New to Reddit? </b>
+            <a href="#">Sign up</a>
+          </div>
+        </form>
+      </div>
+      <div className="submit">
+        <button type="submit" className="login_buttons">
+          Login
+        </button>
+      </div>
     </div>
-  );
+   );
 }
 
 export default Login;
