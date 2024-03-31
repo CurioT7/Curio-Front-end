7fced9f64d8c72d46bc044b5e0b75207aab94319
Yehia Tarek Ragab <94568731+yehiatarek63@users.noreply.github.com>
2024-03-31 15:26:08 +0200

    style: styled post component

--- Curio/src/Components/Post/Post.jsx
+++ Curio/src/Components/Post/Post.jsx
@@ -7,8 +7,33 @@ import { BiUpvote } from "react-icons/bi";
 import { FaRegCommentAlt } from "react-icons/fa";
 import { LuShare } from "react-icons/lu";
 import { SlOptions } from "react-icons/sl";
+import Upvotes from '../../styles/icons/Upvotes.jsx';
+import Downvotes from '../../styles/icons/Downvotes.jsx';
+import FilledDownvote from '../../styles/icons/FilledDownvote.jsx';
+import FilledUpvote from '../../styles/icons/FilledUpvote.jsx';
+import { useState } from 'react';
 import './Post.css'
 function Post(props) {
+    const [upvoted, setUpvoted] = useState(false);
+    const [downvoted, setDownvoted] = useState(false);
+    const makePostUpvoted = () => {
+        if (upvoted) {
+            setUpvoted(false);
+        } else {
+            setUpvoted(true);
+            setDownvoted(false);
+        }
+    }
+    const makePostDownvoted = () => {
+        if (downvoted) {
+            setDownvoted(false);
+        } else {
+            setDownvoted(true);
+            setUpvoted(false);
+        }
+    }
+
+
     return (
         <div>
             <Card className='Post' variant='ghost' >
@@ -18,7 +43,7 @@ function Post(props) {
                         <Avatar size='sm' name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
 
                         <Box>
-                        <h6 className='user-name'>{props.user}</h6>
+                        <a className='community-post-name'>{props.user}</a>
                         
                         </Box>
                     </Flex>
@@ -30,7 +55,7 @@ function Post(props) {
                     />
                     </Flex>
                 </CardHeader>
-                <CardBody>
+                <CardBody className='py-0'>
                     <Heading as='h3' size='md'>{props.title}</Heading>
                     <Text className='text-body'>
                     {props.content}
@@ -44,6 +69,7 @@ function Post(props) {
 
                 <CardFooter
                     display='flex'
+                    className='py-0 pb-2'
                     flexDirection='row'
                     justifyContent='flex-start'
                     flexWrap='wrap'
@@ -53,19 +79,24 @@ function Post(props) {
                     },
                     }}
                 >   
-                    <Box>
-                    
-                        <Button flex='1' variant='ghost' leftIcon={<BiUpvote />}>
-                        {props.upvotes}
+                    <Box display='flex' justifyContent='start'>
+                        <div className='d-flex me-2 align-items-center votes-control px-2' style={{backgroundColor: upvoted ? "#D93A00" : downvoted ? "#6A5CFF" : ""}}>
+                            <button className='me-2 upvotes-footer-button' onClick={() => makePostUpvoted()}>
+                                {upvoted ? <FilledUpvote /> : downvoted ? <Upvotes whiteOutline={true} /> : <Upvotes />}
+                            </button>
+                            <div className='me-2'>
+                                <span className='votes-count' style={{color: upvoted || downvoted ? "#ffffff" : ""}}>{props.upvotes - props.downvotes}</span>
+                            </div>
+                            <button className='downvotes-footer-button' onClick={() => makePostDownvoted()}>
+                                {downvoted ? <FilledDownvote /> : upvoted ? <Downvotes whiteOutline={true} /> : <Downvotes />}
+                            </button>
+                        </div>
+                        <Button flex='1' className='post-footer-button me-2 px-1' variant='ghost' leftIcon={<FaRegCommentAlt />}>
+                        <span className='share-post-text'>{props.comments.length}</span>
                         </Button>
-                        
-                        <Button flex='1' variant='ghost' leftIcon={<BiDownvote />}>
-                        {props.downvotes}
+                        <Button flex='1' className='post-footer-button me-2 px-3' variant='ghost'  leftIcon={<LuShare />}>
+                        <span className='share-post-text'>Share</span>
                         </Button>
-                        <Button flex='1' variant='ghost' leftIcon={<FaRegCommentAlt />}>
-                        {props.comments.length}
-                        </Button>
-                        <Button flex='1' variant='ghost' leftIcon={<LuShare />}/>
                         
                     </Box>
                     
--- Curio/src/Pages/Home/Home.jsx
+++ Curio/src/Pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
 import "./Home.css"
 import RecentPosts from '../../Components/RecentPosts/RecentPosts'
+import Post from '../../Components/Post/Post'
 
 function Home() {
 
@@ -9,6 +11,16 @@ function Home() {
     <>
     <div className='d-flex'>
       {/* Insert posts here (above recent posts) */}
+      <div className='col-md-6 d-flex p-3' style={{marginLeft: "20rem"}}>
+         <Post
+            user="Alice"
+            title="Second Post"
+            content="Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla facilisi. Vivamus id enim odio. Aliquam volutpat urna ac est pulvinar rhoncus."
+            upvotes={10}
+            downvotes={1}
+            comments={[4, 5]} // Dummy array for comments
+          />
+      </div>
       <div className='d-flex justify-content-end ms-auto mb-4 fixed-container' style={{marginRight: "3rem", paddingTop: "1.2rem", height: "100vh", overflowY: "auto", width: "20%"}}>
           <RecentPosts />
       </div>
