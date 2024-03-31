d9588f2f790482dd54d74d260917dddd50815355
MostafaKhaled <MostafaDarwish9392@gmail.com>
2024-03-31 17:32:34 +0200

    fix: :bug: fix top sorting title display

--- Curio/src/Components/CommunitiesListing/Listing.jsx
+++ Curio/src/Components/CommunitiesListing/Listing.jsx
@@ -10,19 +10,6 @@ function Listing(props) {
   function List() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
   function TopBy() {
     document.getElementById("SortTop").classList.toggle("show");
   }
@@ -39,6 +26,8 @@ function Listing(props) {
     }
   }
 
+  
+  
   const { Community } = useParams();
   const [listValue, setListValue] = React.useState('Hot');
   const [community, setCommunity] = React.useState("Community");
@@ -54,11 +43,12 @@ function Listing(props) {
   
   function changeListValue(value) {
     setListValue(value);
-    props.onChangeSort(value);
+    props.onChangeSort(value,sortTop);
   }
   function changeSortTop(value) {
     setSortTop(value);
-   
+    props.onChangeSort("Top",value);
+   console.log(sortTop);
   }
   return (
     <div className="dropdown">
@@ -72,16 +62,20 @@ function Listing(props) {
           <Link to={`/r/${community}/rising`} onClick={() => changeListValue("Random")} className="link-sort">Random</Link>
         </div>
       </div>
-      {/* <div>
+      <div>
+        {listValue === "Top" && <div> 
         <button onClick={TopBy} className="dropbtn"> {sortTop} <IoIosArrowDown className="arrow-icon"/></button>
-        <div id="SortTop" className="dropdown-content">
+        <div id="SortTop" className="dropdown-content ">
           <p className="Sort-title">Sort By</p>
-          <Link to={`/r/${community}/top`}  className="link-sort">Today</Link>
-          <Link to={`/r/${community}/top`}  className="link-sort">Week</Link>
-          <Link to={`/r/${community}/top`}  className="link-sort">Month</Link>
-          <Link to={`/r/${community}/top`}  className="link-sort">Year</Link>
+          <Link to={`/r/${community}/top`} onClick={()=> changeSortTop("Now")}  className="link-sort">Now</Link>
+          <Link to={`/r/${community}/top`} onClick={()=> changeSortTop("Today")}  className="link-sort">Today </Link>
+          <Link to={`/r/${community}/top`} onClick={()=> changeSortTop("This Week")}  className="link-sort">This Week</Link>
+          <Link to={`/r/${community}/top`} onClick={()=> changeSortTop("This Month")}  className="link-sort">This Month</Link>
+          <Link to={`/r/${community}/top`} onClick={()=> changeSortTop("This Year")}  className="link-sort">This Year</Link>
+          <Link to={`/r/${community}/top`} onClick={()=> changeSortTop("All Time")}  className="link-sort">All Time</Link>
         </div>
-      </div> */}
+        </div>}
+      </div>
     </div>
   );
 }
