4f8154fddb7c98943e6c581519646f18f4910161
Yehia Tarek Ragab <94568731+yehiatarek63@users.noreply.github.com>
2024-03-31 22:49:16 +0200

    style: added rich text format

--- Curio/src/Components/Post/CommentInputForm.jsx
+++ Curio/src/Components/Post/CommentInputForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useRef, useEffect } from "react";
 import SwitchToRichFormat from "../../styles/icons/SwitchToRichFormat.jsx";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 
 function CommentInputForm() {
@@ -14,6 +16,15 @@ function CommentInputForm() {
         }
     }, [inputState]);
 
+    const handleSwitchToRich = () => {
+        if(inputState === 1){
+            setInputState(2);
+        }
+        else if (inputState === 2){
+            setInputState(1);
+        }
+    }
+
     return (
         <form className="mb-3">
             <div className="d-flex flex-column">
@@ -26,27 +37,31 @@ function CommentInputForm() {
                         style={{borderRadius: "20px"}}
                     />
                 }
-                {inputState === 1 && 
-                    <div className="d-flex flex-column border-1 border-secondary" style={{borderRadius: "20px"}}>
-                        <textarea
-                            ref={textareaRef}
-                            onFocus={() => setInputState(1)}
-                            type="text"
-                            className="form-control textarea-comment p-2 px-3 border-0"
-                            placeholder="Add a comment"
-                            style={{borderRadius: "20px"}}
-                        />
-                        <div className="d-flex justify-content-between">
-                            <div className="me-auto ms-3 d-flex align-items-center">
-                                <button className="switch-rich p-2"><SwitchToRichFormat /></button>
+                    {(inputState === 1 || inputState === 2) && 
+                        <div className="d-flex flex-column border-1" style={{borderRadius: "20px"}}>
+                            {inputState === 1 && <textarea
+                                ref={textareaRef}
+                                onFocus={() => setInputState(1)}
+                                type="text"
+                                className="form-control textarea-comment p-2 px-3 border-0"
+                                placeholder="Add a comment"
+                                style={{borderRadius: "20px"}}
+                            />}
+                            {inputState === 2 && <ReactQuill className="border-0" theme="snow" />}
+                            {(inputState === 1 || inputState === 2) && <div className="d-flex justify-content-between">
+                                <div className="me-auto ms-3 d-flex align-items-center">
+                                    <button type="button" onClick={handleSwitchToRich} style={{backgroundColor: (inputState === 2) ? "#D2DADD" : ""}} className="switch-rich p-2"><SwitchToRichFormat /></button>
+                                </div>
+                                <div className="d-flex ms-auto mb-2">
+                                    <button onClick={() => setInputState(0)} className="me-3 cancel-comment-button p-2">Cancel</button>
+                                    <button className="me-4 comment-button-post-details p-2">Comment</button>
+                                </div>
                             </div>
-                            <div className="d-flex ms-auto mb-2">
-                                <button onClick={() => setInputState(0)} className="me-3 cancel-comment-button p-2">Cancel</button>
-                                <button className="me-4 comment-button-post-details p-2">Comment</button>
-                            </div>
+                            }
+                        
                         </div>
-                    </div>
-                }
+                    }
+                    
 
             </div>
         </form>
