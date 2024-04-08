f7942b84ff47d2da1732b1a3274ba35c257e4434
MostafaKhaled <MostafaDarwish9392@gmail.com>
2024-04-08 23:42:47 +0200

    fix: :ambulance: Delete fix axios request
    delete method  must handled in different way by using data config

--- Curio/src/Components/accountSettings/buttons/DeleteButton.jsx
+++ Curio/src/Components/accountSettings/buttons/DeleteButton.jsx
@@ -60,15 +60,16 @@ function DeleteButton(){
     async function sendDataToBackend(){
         console.log(`Bearer ${localStorage.getItem('token')}`)
         try {
-            const response = await axios.delete(`${serverHost}/api/settings/delete_account`, {
+            const response = await axios.delete(`${serverHost}/api/settings/delete_account`,{data: {
                 username: userName,
                 password: yourPass,
-            }, {
+            }, 
                 headers: {
                     authorization: `Bearer ${localStorage.getItem('token')}`
                 }
             });
             navigate('/')
+            localStorage.removeItem('token')
             clearForm()
         }
         catch(error){
