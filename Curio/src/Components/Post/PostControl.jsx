e4307d175d31ca538a94e516bb40f389107efe9e
Yehia Tarek Ragab <94568731+yehiatarek63@users.noreply.github.com>
2024-04-08 23:24:31 +0200

    style: added extra modal

--- Curio/src/Components/Post/PostControl.jsx
+++ Curio/src/Components/Post/PostControl.jsx
@@ -4,7 +4,8 @@ import Ellipsis from "../../styles/icons/Elippsis";
 import SaveButton from "../../styles/icons/SaveButton";
 import ReportPost from "../../styles/icons/ReportPost";
 import Hide from "../../styles/icons/Hide";
-import ReportReason  from "./ReportReason";
+import ReportReason  from "./ReportReason.jsx";
+import ReportSubmitted from "./ReportSubmitted.jsx";
 
 
 function PostControl(props) {
@@ -24,45 +25,46 @@ function PostControl(props) {
   }
   return (
     <>
-    <div>
-      <button className="post-dropdown-control d-flex justify-content-center align-items-center" onClick={handleEllipsisClick}>
-        <Ellipsis className="ellipsis-img" />
-      </button>
-      {showControls && <div className="post-dropdown" style={{ 
-                                        display: showControls ? 'flex' : 'none',
-                                        flexDirection: 'column',
-                                        alignItems: 'flex-start',
-                                        position: 'absolute',
-                                        top: '0',
-                                        right: '0',
-                                        backgroundColor: '#FFFFFF',
-                                        borderRadius: '10px',
-                                        boxShadow: '2px 6px 10px rgba(0, 0, 0, 0.4)',
-                                        marginTop: '2.5rem',
-                                        marginRight: '1rem',
-                                        padding: '0px',
-                                        width: '0.5rem!important',
-                                        height: '0.5rem!important',
-                                        zIndex: '1'
-                                    }}>
-                                        <ul className='drop-down-list w-100 px-0'>
-                                            <li className="drop-down-item ps-3 dropdown-list-post-control d-flex align-items-center">
-                                                    <SaveButton />
-                                                    <div className="d-flex align-items-center justify-content-center"><p className='mt-3 text-text d-flex'>Save</p></div>
-                                            </li>
-                                            <li className="drop-down-item ps-3 dropdown-list-post-control d-flex align-items-center">
-                                                    <Hide />
-                                                    <div><p className='mt-3 text-text'>Hide</p></div>
-                                            </li>
-                                            <li onClick={handleOpenReportModal} className="drop-down-item ps-3 dropdown-list-post-control d-flex align-items-center">
-                                                    <ReportPost />
-                                                    <div><p className='mt-3 text-text'>Report</p></div>
-                                            </li>
-                                        </ul>   
-                                    </div>
-            }
-    </div>
-    <ReportReason show={isReportReasonModalOpen} showSubmittedReport={handleShowSubmittedReport} onHide={() => setReportReasonModalOpen(false)} />
+      <div>
+        <button className="post-dropdown-control d-flex justify-content-center align-items-center" onClick={handleEllipsisClick}>
+          <Ellipsis className="ellipsis-img" />
+        </button>
+        {showControls && <div className="post-dropdown" style={{ 
+                                          display: showControls ? 'flex' : 'none',
+                                          flexDirection: 'column',
+                                          alignItems: 'flex-start',
+                                          position: 'absolute',
+                                          top: '0',
+                                          right: '0',
+                                          backgroundColor: '#FFFFFF',
+                                          borderRadius: '10px',
+                                          boxShadow: '2px 6px 10px rgba(0, 0, 0, 0.4)',
+                                          marginTop: '2.5rem',
+                                          marginRight: '1rem',
+                                          padding: '0px',
+                                          width: '0.5rem!important',
+                                          height: '0.5rem!important',
+                                          zIndex: '1'
+                                      }}>
+                                          <ul className='drop-down-list w-100 px-0'>
+                                              <li className="drop-down-item ps-3 dropdown-list-post-control d-flex align-items-center">
+                                                      <SaveButton />
+                                                      <div className="d-flex align-items-center justify-content-center"><p className='mt-3 text-text d-flex'>Save</p></div>
+                                              </li>
+                                              <li className="drop-down-item ps-3 dropdown-list-post-control d-flex align-items-center">
+                                                      <Hide />
+                                                      <div><p className='mt-3 text-text'>Hide</p></div>
+                                              </li>
+                                              <li onClick={handleOpenReportModal} className="drop-down-item ps-3 dropdown-list-post-control d-flex align-items-center">
+                                                      <ReportPost />
+                                                      <div><p className='mt-3 text-text'>Report</p></div>
+                                              </li>
+                                          </ul>   
+                                      </div>
+              }
+      </div>
+      <ReportReason show={isReportReasonModalOpen} showSubmittedReport={handleShowSubmittedReport} onHide={() => setReportReasonModalOpen(false)} />
+      <ReportSubmitted show={isReportSubmittedModalOpen} onHide={() => setReportSubmittedModalOpen(false)} />
     </>
   );
 }
--- Curio/src/Components/Post/ReportSubmitted.jsx
+++ Curio/src/Components/Post/ReportSubmitted.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import CloseButton from '../../styles/icons/CloseButton.jsx';
+import ReportIcon from "../../styles/icons/ReportIcon.jsx";
+import axios from "axios";
+import './ReportModals.css';
 
 function ReportSubmitted(props) {
   return (
@@ -10,7 +16,7 @@ function ReportSubmitted(props) {
       contentClassName="report-div-modal"
       centered
     >
-      <Modal.Header className="border-0 pb-0">
+      <Modal.Header className="border-0 pb-0" style={{marginBottom: "10rem"}}>
         <Modal.Title className="d-flex flex-column w-100" id="contained-modal-title-vcenter">
           <div className="d-flex w-100">
             <h2 className="submit-a-report-header">Report Submitted</h2>
@@ -21,18 +27,24 @@ function ReportSubmitted(props) {
           </div>
           <div>
             <p className="report-reason-paragraph">Thanks for looking out for yourself and your fellow redditors by reporting things that break the rules. Let us know what's happening, and we'll look into it. </p>
-          </div>jk
+          </div>
         </Modal.Title>
       </Modal.Header>
-      <Modal.Footer className="d-flex flex-column border-0">
-        <div className="d-flex w-100">
-          <div className="d-flex flex-column col-md-9">
-            <h2 style={{fontSize: "1rem"}} className="submit-a-report-header">Block ancient_gate_007</h2>
+      <Modal.Footer className="d-flex flex-column border-0 mt-5">
+        <div className="d-flex w-100 d-flex flex-column">
+          <div className="d-flex w-100 mb-3">
+            <h2 style={{fontSize: "1rem"}} className="submit-a-report-header d-flex align-items-center">Block ancient_gate_007</h2>
+            <div className='form-check form-switch d-flex align-items-center ms-auto'>
+                <input style={{ transform: 'scale(1.5)' }} className='form-check-input ms-auto me-3' type="checkbox" id="mature" role="switch" name="mature" value="mature" />
+            </div>
           </div>
-          <div className="ms-auto d-flex">
+          <div>
+            <p className="block-warning-secondary">You won't be able to send direct messages or chat requests to each other.</p>
+          </div>
+          <div className="ms-auto">
             <button
-             onClick={handleReportSubmit}
              className="report-reason-next-button-enabled"
+              onClick={props.onHide}
             >Done</button>
           </div>
         </div>
