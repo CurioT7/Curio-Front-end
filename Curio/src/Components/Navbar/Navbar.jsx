b402f1f8c50af049ce14dddae38a22bda3bb21e8
omar-adel1 <omaradelhassan12@gmail.com>
2024-04-10 20:31:04 +0200

    update

--- Curio/src/Components/Navbar/Navbar.jsx
+++ Curio/src/Components/Navbar/Navbar.jsx
@@ -7,63 +7,12 @@ import plus from "../../assets/Plus_navbar.png";
 import inbox from "../../assets/Inbox_navbar.png";
 import profile from "../../assets/Profile_navbar.png";
 import setting from "../../assets/Setting_navbar.png";
-// import search_icon from "../../assets/search_icon.png"
 import { Tooltip } from "@chakra-ui/react";
 import { Link } from 'react-router-dom';
 import SignupHandler from './SignupHandler';
-// import { Navbar, Container, Nav, NavDropdown, Form, Button, Offcanvas } from 'react-bootstrap';
-// import Avatar from '../../styles/icons/Avatar';
-// import SearchInput from "./SearchInput"
 
 function NavbarComponent() {
   const username = localStorage.getItem('username');
-  // return (
-  //   <>
-  //     {['lg'].map((expand) => (
-  //       <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3" style={{zIndex:"1000"}}>
-  //         <Container fluid>
-  //           <Navbar.Brand href="/"><img src={logo} alt="logo" className="d-inline-block align-text-middle"/> Curio</Navbar.Brand>
-  //           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-  //           <Navbar.Offcanvas
-  //             id={`offcanvasNavbar-expand-${expand}`}
-  //             aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-  //             placement="end"
-  //           >
-  //             <Offcanvas.Header closeButton>
-  //               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-  //                 Offcanvas
-  //               </Offcanvas.Title>
-  //             </Offcanvas.Header>
-  //             <Offcanvas.Body className='d-flex justify-content-center'>
-  //               <Nav className='d-flex justify-content-end search-offcanvas w-100'>
-  //                   <Form className="d-flex mt-1">
-  //                     <SearchInput/>
-  //                   </Form>
-  //               </Nav>
-  //               <Nav className="d-flex ms-auto flex-grow-1 w-50">
-  //                 <Nav.Link style={{height: "30px", marginTop: "10px"}} className='ms-auto d-flex align-items-center me-2 link-offcanvas' href="#action3"><img src={openchat} alt="profile" className='inbox-icon' /></Nav.Link>
-  //                 <Nav.Link style={{height: "40px"}} className='mt-2 me-2 link-offcanvas' href="#action3"><img src={inbox} alt="profile" className='inbox-icon' /></Nav.Link>
-  //                 <Nav.Link style={{ height: "40px", display: "flex", alignItems: "center" }} className='mt-2 me-2 link-offcanvas'>
-  //                     <Link to={'user/CreatePost/'} style={{ display: "flex", alignItems: "center" }}>
-  //                         <img src={plus} alt="profile" className='inbox-icon' style={{ marginRight: "5px" }} />
-  //                         Create
-  //                     </Link>
-  //                 </Nav.Link>
-  //                 <NavDropdown align={{ lg: 'end' }} style={{borderRadius: '999px!important', backgroundColor: "#ffffff!important", color:"#000000!important"}} className="signup-button px-2 me-4 ms-3 d-flex justify-content-center col-md-1 col-xs-1 link-offcanvas" title="Profile">
-  //                   <NavDropdown.Item className="d-flex signup-button-item px-3"><span className="ms-3"> <Link style={{color: "#000000!important"}} className='settings' to={`user/${username}`} > View Profile </Link> </span></NavDropdown.Item>
-  //                   <NavDropdown.Item style={{height: "50px", color: "#000000!important"}} className="d-flex align-items-center signup-button-item px-2 link-offcanvas"><Link style={{color: "#000000!important"}} className='settings' to={'settings/account'}><div style={{backgroundColor: "#f2f4f5!important"}} className='d-flex'><img style={{height: "20px"}} src={setting} alt="setting" className="settings-offcanvas align-text-middle mx-2"/><span>View Settings</span></div></Link></NavDropdown.Item>
-  //                 </NavDropdown>
-  //                 <div className='d-flex align-items-center'>
-  //                   <SignupHandler/>
-  //                 </div>
-  //               </Nav>         
-  //             </Offcanvas.Body>
-  //           </Navbar.Offcanvas>
-  //         </Container>
-  //       </Navbar>
-  //     ))}
-  //   </>
-  // );
   
   function toggleMenu(){
     let subMenu = document.getElementById("subMenu");
