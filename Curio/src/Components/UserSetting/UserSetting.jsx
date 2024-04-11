e42a203189a7162d8f24ec2a4574116d0c475979
omar-adel1 <omaradelhassan12@gmail.com>
2024-04-11 18:33:58 +0200

    update

--- Curio/src/Components/UserSetting/UserSetting.jsx
+++ Curio/src/Components/UserSetting/UserSetting.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 
 
 function UserSetting() {
-  const [selectedTabsetting, setSelectedTabSetting] = useState("Account");
+  // const [selectedTabsetting, setSelectedTabSetting] = useState("Account");
 
-  const handleButtonClickSetting = (buttonName) => {
-    // e.preventDefault();
-    setSelectedTabSetting(buttonName);
-  };
+  // const handleButtonClickSetting = (buttonName) => {
+  //   // e.preventDefault();
+  //   setSelectedTabSetting(buttonName);
+  // };
 
   return (
     <div className='container'>
@@ -21,78 +21,78 @@ function UserSetting() {
             to={'/settings/account'} 
             className="setting-link navbar-brand" 
             id="account-link" 
-            onClick={() => handleButtonClickSetting("Account")}
-            style={{
-              color: selectedTabsetting === "Account" ? "black" : "",
-              borderBottomColor: selectedTabsetting === "Account" ? "#0079d3" : "",
-              borderWidth: selectedTabsetting === "Account" ? '0 0 2px 0' : '0',
-            }}
+            // onClick={() => handleButtonClickSetting("Account")}
+            // style={{
+            //   color: selectedTabsetting === "Account" ? "black" : "",
+            //   borderBottomColor: selectedTabsetting === "Account" ? "#0079d3" : "",
+            //   borderWidth: selectedTabsetting === "Account" ? '0 0 2px 0' : '0',
+            // }}
             >Account</Link>
             <Link 
             to={'/settings/profile'} 
             className="setting-link navbar-brand" 
             id="profile-link" 
-            onClick={() => handleButtonClickSetting("Profile")}
-            style={{
-              color: selectedTabsetting === "Profile" ? "black" : "",
-              borderBottomColor: selectedTabsetting === "Profile" ? "#0079d3" : "",
-              borderWidth: selectedTabsetting === "Profile" ? '0 0 2px 0' : '0',
-            }}
+            // onClick={() => handleButtonClickSetting("Profile")}
+            // style={{
+            //   color: selectedTabsetting === "Profile" ? "black" : "",
+            //   borderBottomColor: selectedTabsetting === "Profile" ? "#0079d3" : "",
+            //   borderWidth: selectedTabsetting === "Profile" ? '0 0 2px 0' : '0',
+            // }}
             >Profile</Link>
             <Link 
             to={'/settings/privacy'} 
             className="setting-link navbar-brand" 
             id="safety-privacy-link"
-            onClick={() => handleButtonClickSetting("Safety & Privacy")}
-            style={{
-              color: selectedTabsetting === "Safety & Privacy" ? "black" : "",
-              borderBottomColor: selectedTabsetting === "Safety & Privacy" ? "#0079d3" : "",
-              borderWidth: selectedTabsetting === "Safety & Privacy" ? '0 0 2px 0' : '0',
-            }}
+            // onClick={() => handleButtonClickSetting("Safety & Privacy")}
+            // style={{
+            //   color: selectedTabsetting === "Safety & Privacy" ? "black" : "",
+            //   borderBottomColor: selectedTabsetting === "Safety & Privacy" ? "#0079d3" : "",
+            //   borderWidth: selectedTabsetting === "Safety & Privacy" ? '0 0 2px 0' : '0',
+            // }}
             >Safety & Privacy</Link>
             <Link 
             to={'/settings/feeding'} 
             className="setting-link navbar-brand" 
             id="feed-settings-link"
-            onClick={() => handleButtonClickSetting("Feed Settings")}
-            style={{
-              color: selectedTabsetting === "Feed Settings" ? "black" : "",
-              borderBottomColor: selectedTabsetting === "Feed Settings" ? "#0079d3" : "",
-              borderWidth: selectedTabsetting === "Feed Settings" ? '0 0 2px 0' : '0',
-            }}
+            // onClick={() => handleButtonClickSetting("Feed Settings")}
+            // style={{
+            //   color: selectedTabsetting === "Feed Settings" ? "black" : "",
+            //   borderBottomColor: selectedTabsetting === "Feed Settings" ? "#0079d3" : "",
+            //   borderWidth: selectedTabsetting === "Feed Settings" ? '0 0 2px 0' : '0',
+            // }}
             >Feed Settings</Link>
             <Link 
             to={'/settings/notifications'} 
             className="setting-link navbar-brand" 
             id="notifications-link"
-            onClick={() => handleButtonClickSetting("Notifications")}
-            style={{
-              color: selectedTabsetting === "Notifications" ? "black" : "",
-              borderBottomColor: selectedTabsetting === "Notifications" ? "#0079d3" : "",
-              borderWidth: selectedTabsetting === "Notifications" ? '0 0 2px 0' : '0',
-            }}
+            // onClick={() => handleButtonClickSetting("Notifications")}
+            // style={{
+            //   color: selectedTabsetting === "Notifications" ? "black" : "",
+            //   borderBottomColor: selectedTabsetting === "Notifications" ? "#0079d3" : "",
+            //   borderWidth: selectedTabsetting === "Notifications" ? '0 0 2px 0' : '0',
+            // }}
             >Notifications</Link>
             <Link 
             to={'/settings/email'} 
             className="setting-link navbar-brand" 
             id="emails-link"
-            onClick={() => handleButtonClickSetting("Emails")}
-            style={{
-              color: selectedTabsetting === "Emails" ? "black" : "",
-              borderBottomColor: selectedTabsetting === "Emails" ? "#0079d3" : "",
-              borderWidth: selectedTabsetting === "Emails" ? '0 0 2px 0' : '0',
-            }}
+            // onClick={() => handleButtonClickSetting("Emails")}
+            // style={{
+            //   color: selectedTabsetting === "Emails" ? "black" : "",
+            //   borderBottomColor: selectedTabsetting === "Emails" ? "#0079d3" : "",
+            //   borderWidth: selectedTabsetting === "Emails" ? '0 0 2px 0' : '0',
+            // }}
             >Emails</Link>
             <Link 
             to={'/settings/chatandmasseging'} 
             className="setting-link navbar-brand" 
             id="chat-messaging-link"
-            onClick={() => handleButtonClickSetting("Chat & Messaging")}
-            style={{
-              color: selectedTabsetting === "Chat & Messaging" ? "black" : "",
-              borderBottomColor: selectedTabsetting === "Chat & Messaging" ? "#0079d3" : "",
-              borderWidth: selectedTabsetting === "Chat & Messaging" ? '0 0 2px 0' : '0',
-            }}
+            // onClick={() => handleButtonClickSetting("Chat & Messaging")}
+            // style={{
+            //   color: selectedTabsetting === "Chat & Messaging" ? "black" : "",
+            //   borderBottomColor: selectedTabsetting === "Chat & Messaging" ? "#0079d3" : "",
+            //   borderWidth: selectedTabsetting === "Chat & Messaging" ? '0 0 2px 0' : '0',
+            // }}
             >Chat & Messaging</Link>
           </div>
         </div>
