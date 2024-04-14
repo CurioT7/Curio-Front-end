986ca1ff4d35a1cd61b6bc9d4a7f6c4acb803aff
Yehia Tarek Ragab <94568731+yehiatarek63@users.noreply.github.com>
2024-04-14 21:13:52 +0200

    handle gender

--- Curio/src/Components/Login/SignupHandlerForLogin.jsx
+++ Curio/src/Components/Login/SignupHandlerForLogin.jsx
@@ -18,6 +18,7 @@ function SignupHandlerForLogin() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [username, setEnteredUsername] = useState('');
   const [password, setEnteredPassword] = useState('');
+  const [enteredGender, setEnteredGender] = useState('');
   const [isPreferencesModalOpen, setPreferencesModalOpen] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate();
@@ -87,7 +88,7 @@ function SignupHandlerForLogin() {
 
   const handleSignup = async () => {
     try{
-      const response = await signup({username, email: enteredEmail, password});
+      const response = await signup({username, email: enteredEmail, password, gender: enteredGender});
       console.log(response);
       if(response.status === 201){
         console.log('Signup successful');
@@ -107,6 +108,9 @@ function SignupHandlerForLogin() {
     }
   }
 
+  const handleSettingGender = (gender) => {
+    setEnteredGender(gender);
+  }
 
 
   const handleLogout = () => {
@@ -122,7 +126,7 @@ function SignupHandlerForLogin() {
             <button style={{color: "#0045ac"}} onClick={handleSignupInfoClick}>Signup</button>
             {isSignupInfoModalOpen && <SignupInfo show={isSignupInfoModalOpen} onHide={() => setSignupInfoModalOpen(false)} onContinue={handleOpenUsernameInfo} onEnteredEmail={handleEnteredEmail} enteredEmail={enteredEmail} />}
             {isUsernameInfoModalOpen && <UsernameInfo show={isUsernameInfoModalOpen} onHide={() => setUsernameInfoModalOpen(false)} onContinueToGender={handleContinueToGender} onEnteredUsername={handleEnteredUsername} onEnteredPassword={handleEnteredPassword} enteredUsername={username} enteredPassword={password} onBack={handleBackToSignupInfo} />}
-            {isGenderModal && <Gender show={isGenderModal} onHide={() => setGenderModalOpen(false)} onContinueToPreferences={handleContinueToPreferences} />}
+            {isGenderModal && <Gender show={isGenderModal} handleEnteredGender={handleSettingGender} onHide={() => setGenderModalOpen(false)} onContinueToPreferences={handleContinueToPreferences} />}
             {isPreferencesModalOpen && <Preferences show={isPreferencesModalOpen} onHide={() => setPreferencesModalOpen(false)} onBackToGender={handleBackToGender} onSignup={handleSignup} />}
         </>
     );
