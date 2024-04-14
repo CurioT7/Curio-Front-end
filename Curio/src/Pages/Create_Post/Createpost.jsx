00d3d48adf55b0b2962930662d62837ec44d2ef3
omar-adel1 <omaradelhassan12@gmail.com>
2024-04-14 14:17:44 +0200

    "Update Createpost.css and Createpost.jsx with new styles and state management; add Icons component to Text_Editor; update Socialmodal to handle social link display names and platforms; update Advanced to fetch user data"

--- Curio/src/Pages/Create_Post/Createpost.jsx
+++ Curio/src/Pages/Create_Post/Createpost.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect } from "react";
+import React, { useState, useEffect  } from "react";
 import {useNavigate} from 'react-router-dom';
 import { Box, Button, Text } from '@chakra-ui/react';
 import NewPostForm from "../../Components/Create_Post/NewPostForm";
