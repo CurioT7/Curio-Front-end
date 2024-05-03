819e608766c82f6d20f8514524cd4a0d13b92163
omar-adel1 <omaradelhassan12@gmail.com>
2024-05-04 01:13:44 +0300

    "Update Sent Messages page to display messages and modify Sent Messages Table component"
    This commit updates the Sent Messages page to display messages fetched from the backend and modifies the Sent Messages Table component to accept

--- Curio/src/Components/Sent_Messages_Table/Sent_Messages_Table.jsx
+++ Curio/src/Components/Sent_Messages_Table/Sent_Messages_Table.jsx
@@ -1,16 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import "./Sent_Messages_Table.css";
+import { getTimeDifference } from '../getTimeDifference/getTimeDifference';
+import { Link } from 'react-router-dom';
 
-
-function Sent_Messages() {
+function Sent_Messages(props) {
     const username = localStorage.getItem('username');
+    const { isRecipientNull } = props;
 
     return (
         <div className='sent_message_table_content'>
-            <h5 className='subject_sent_messages'>Subject:</h5>
-            <div style={{display:'flex', flexDirection:'column', gap:'18px'}}>
-            <span className='sent_message_details'>to <a href="">/u/"to"</a> via /r/"fromsubreddit" <span className='sent_message_time'></span>sent 43 minutes ago</span>
-            <span className='message_name_detail'>Message</span>
+            <h5 className='subject_sent_messages'>{props.subject}:</h5>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '18px' }}>
+                <span className='sent_message_details'>to
+                    <a href="" style={{
+                        color: "#80bce9"
+                    }}>
+                        {isRecipientNull === true ?
+                            "/u/" : "/r/"
+                        }{props.recipient}
+                    </a>
+                    {props.sender !== username &&
+                        <>
+                            <span> via </span>
+                            <Link to={`/r/${props.sender}`} style={{ color: '#228822' }}>
+                                /r/{props.sender}
+                            </Link>
+                            <span style={{marginLeft:'5px'}}>[
+                                <Link to={`/r/${props.sender}`} style={{color:'#228822'}}>M</Link>
+                            </span>]
+                        </>
+                    }
+                    <span className='sent_message_time'></span>sent {getTimeDifference(props.timestamp)} ago
+                </span>
+                <span className='message_name_detail'>{props.message}</span>
             </div>
         </div>
     );
--- Curio/src/Pages/Sent_Messages/Sent_Messages.jsx
+++ Curio/src/Pages/Sent_Messages/Sent_Messages.jsx
@@ -8,7 +8,8 @@ const serverHost = import.meta.env.VITE_SERVER_HOST;
 
 function Sent_Messages(props) {
     const navigate = useNavigate();
-    const username = localStorage.getItem('username');
+    const [sentMessages, setSentMessages] = useState([]);
+
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -16,17 +17,58 @@ function Sent_Messages(props) {
             navigate('/login');
         }
         props.hideSidebar();
-        // fetchUserData();
+        fetchUserData();
         return () => {
             props.showSidebar();
         }
     }, []);
 
+    const fetchUserData = async () => {
+        try {
+            const response = await axios.get(`${serverHost}/api/message/sent`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            });
+            setSentMessages(response.data.messages);
+        } catch (error) {
+            if (error.response) {
+                if (error.response.status === 500) {
+                    console.error('Error retrieving search results');
+                }
+            } else {
+                console.error('Network Error. Please check your internet connection.');
+            }
+        }
+    };
+
     return (
         <div className='sent_message_content'>
-            <div className='sent_message_table'>
-                <Sent_Messages_Table />
+            <div 
+            style={{ 
+                marginTop: '70px', 
+                marginBottom:'70px',
+                width: '70%',
+                }}>
+                {sentMessages.map((message, index) => (
+                    <div key={message._id}>
+                        <div className='sent_message_table' 
+                            style={{
+                                background: index % 2 === 0 ? "#f6f7f8" : "white"
+                            }}>
+                            <Sent_Messages_Table
+                                subject={message.subject}
+                                message={message.message}
+                                timestamp={message.timestamp}
+                                sender={message.sender !== null ? message.sender.username: message.senderSubreddit.name}
+                                recipient={message.recipient !==null ? message.recipient.username: message.recipientSubreddit.name}
+                                isRecipientNull={!message.recipient}
+                                 />
+                        </div>
+                    </div>
+                ))}
             </div>
+
         </div>
     );
 }
