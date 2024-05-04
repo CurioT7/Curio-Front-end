bcf9b307f7c132a85c7cb5f1cdd236a3d76ececf
omar-adel1 <omaradelhassan12@gmail.com>
2024-05-04 23:00:55 +0300

    "Update LiveChat component with new design and add date line; update NewChat component to allow selecting multiple users and display group name input"
    This commit includes updates to the LiveChat component with a new design, including a date line and updated

--- Curio/src/Components/OpenChat/OpenChatComRight_Side/LiveChat/LiveChat.jsx
+++ Curio/src/Components/OpenChat/OpenChatComRight_Side/LiveChat/LiveChat.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import "./LiveChat.css";
 import profile from "../../../../assets/avatar_default_6.png";
 import HeaderChatRight_Side from "../../HeaderChatRight_Side/HeaderChatRight_Side";
@@ -10,7 +10,6 @@ import data from '@emoji-mart/data';
 
 function LiveChat() {
     const [isPickerVisible, setPickerVisible] = useState(false);
-    const [currentEmoji, setCurrentEmoji] = useState(null);
     const [message, setMessage] = useState('');
 
     const pickerRef = useRef(null);
@@ -20,7 +19,6 @@ function LiveChat() {
     };
 
     const handleEmojiSelect = (emoji) => {
-        setCurrentEmoji(emoji.native);
         setMessage(message + emoji.native);
     };
 
@@ -46,6 +44,34 @@ function LiveChat() {
                             Redditor for 61d  ·  1 karma
                         </div>
                     </a>
+                </div>
+            </div>
+            <div className='message-date-live-chat'>
+                <div className='date-line-beside' />
+                Apr 28
+                <div className='date-line-beside' />
+            </div>
+            <div className='message-content-live-chat-container'>
+                <span className='image-chat-message'>
+                    <img src={profile} alt="" style={{ borderRadius: '20px' }} />
+                </span>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '0.5em'
+                }}>
+                    <div style={{
+                        display:'flex',
+                        flexDirection:'row',
+                        gap: '.25rem',
+                        alignItems: 'center'
+                    }}>
+                        <span className='sender-name-live-chat'>General_Boat_962</span>
+                        <span className='sender-time-live-chat'>1:04 AM</span>
+                    </div>
+                    <span>
+                        Hi, I am Omar Adel
+                    </span>
                 </div>
             </div>
             <div className='chat-live-input'>
@@ -79,32 +105,33 @@ function LiveChat() {
                                 onChange={handleMessageChange}
                             />
                             <InputRightElement width='4.5rem'>
-                            <Tooltip label='Select sticker' placement='top'>
-                                <Button h='1.75rem' size='sm' colorScheme='teal' variant='ghost'
-                                    style={{
-                                        paddingRight: '8px',
-                                        paddingLeft: '8px',
-                                        height: '40px',
-                                        width: '40px',
-                                        borderRadius: '100%'
-                                    }}
-                                    onClick={() => setPickerVisible(!isPickerVisible)}>
-                                    <BsFillEmojiSmileFill style={{
-                                        height: '40px',
-                                        width: '40px',
-                                    }} />
-                                </Button>
+                                <Tooltip hasArrow label='Select sticker' placement='top'>
+                                    <Button h='1.75rem' size='sm' colorScheme='teal' variant='ghost'
+                                        style={{
+                                            paddingRight: '8px',
+                                            paddingLeft: '8px',
+                                            height: '40px',
+                                            width: '40px',
+                                            borderRadius: '100%'
+                                        }}
+                                        onClick={() => setPickerVisible(!isPickerVisible)}>
+                                        <BsFillEmojiSmileFill style={{
+                                            height: '40px',
+                                            width: '40px',
+                                        }} />
+                                    </Button>
                                 </Tooltip>
                             </InputRightElement>
                         </InputGroup>
                     </div>
-                    <Button colorScheme='blue' variant='ghost'
+                    <Button colorScheme={message ? 'blue' : 'gray'} variant={message ? 'ghost' : 'none'}
                         style={{
                             paddingRight: '8px',
                             paddingLeft: '8px',
                             height: '40px',
                             width: '40px',
-                            borderRadius: '100%'
+                            borderRadius: '100%',
+                            cursor: message ? 'pointer' : 'default'
                         }}>
                         <IoMdSend style={{
                             height: '40px',
--- Curio/src/Components/OpenChat/OpenChatComRight_Side/NewChat/NewChat.jsx
+++ Curio/src/Components/OpenChat/OpenChatComRight_Side/NewChat/NewChat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import profile from "../../../../assets/avatar_default_6.png";
 import { FaRegSquare } from "react-icons/fa";
 import { BsCheckSquareFill } from "react-icons/bs";
@@ -6,20 +6,21 @@ import "./NewChat.css";
 import { Image, Button, Stack, Spacer } from '@chakra-ui/react';
 import HeaderChatRight_Side from "../../HeaderChatRight_Side/HeaderChatRight_Side";
 
-
 function NewChat() {
-    const [squareIsChecked, setSquareIsChecked] = useState(false);
-    const [selectedUser, setSelectedUser] = useState({ image: '', username: '' });
+    const [selectedUsers, setSelectedUsers] = useState([]);
 
-    const handleSquareIsCheckedAndSelectUser = (image, username) => {
-        setSquareIsChecked(!squareIsChecked);
-        setSelectedUser({ image, username });
-        if (!squareIsChecked) {
-            document.getElementById("floatingInput").value = username;
+    const handleToggleUser = (image, username) => {
+        const isUserSelected = selectedUsers.some(user => user.username === username);
+        if (isUserSelected) {
+            const updatedUsers = selectedUsers.filter(user => user.username !== username);
+            setSelectedUsers(updatedUsers);
         } else {
-            document.getElementById("floatingInput").value = "";
+            setSelectedUsers([...selectedUsers, { image, username }]);
         }
     };
+
+
+    const inputValue = selectedUsers.map(user => user.username).join(', ');
 
     return (
         <div className='chat-div'>
@@ -29,13 +30,35 @@ function NewChat() {
                     <div>
                         <span>
                             <div>
-                                <div class="form-floating">
-                                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" style={{ borderRadius: '1rem' }}/>
-                                    <label for="floatingInput">Type username(s)<span style={{color:'red'}}>*</span></label>
+                                <div className="form-floating" style={{ marginBottom: '.5rem' }}>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="floatingInput"
+                                        style={{
+                                            borderRadius: '1rem',
+                                            background: 'transparent',
+                                            transition: 'background 0.3s'
+                                        }}
+                                        onMouseEnter={(e) => e.target.style.background = '#eaedefb2'}
+                                        onMouseLeave={(e) => e.target.style.background = 'transparent'}
+                                    />
+                                    <label htmlFor="floatingInput">Group Name<span style={{ color: 'red' }}>*</span></label>
+                                </div>
+
+                                <div className="form-floating">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="floatingInput"
+                                        style={{ borderRadius: '1rem' }}
+                                        value={inputValue}
+                                    />
+                                    <label htmlFor="floatingInput">Type username(s)<span style={{ color: 'red' }}>*</span></label>
                                 </div>
                                 <div className='new-chat-form-container'>
                                     <div className='suggested-div'>Suggested</div>
-                                    <li className='chat-li' onClick={() => handleSquareIsCheckedAndSelectUser({profile}, 'General_Boat_962')} >
+                                    <li className='chat-li' onClick={() => handleToggleUser(profile, 'General_Boat_962')} >
                                         <div className='container-li-chat'>
                                             <span className='span-li-chat'>
                                                 <Image
@@ -57,7 +80,33 @@ function NewChat() {
                                                     alignItems: 'center'
                                                 }}
                                             >
-                                                {squareIsChecked ? <BsCheckSquareFill /> : <FaRegSquare />}
+                                                {selectedUsers.some(user => user.username === 'General_Boat_962') ? <BsCheckSquareFill /> : <FaRegSquare />}
+                                            </span>
+                                        </div>
+                                    </li>
+                                    <li className='chat-li' onClick={() => handleToggleUser(profile, 'sui')} >
+                                        <div className='container-li-chat'>
+                                            <span className='span-li-chat'>
+                                                <Image
+                                                    boxSize='32px'
+                                                    objectFit='cover'
+                                                    src={profile}
+                                                    alt='Dan Abramov'
+                                                    style={{
+                                                        display: 'flex',
+                                                        alignItems: 'center',
+                                                        borderRadius: '20px'
+                                                    }}
+                                                />
+                                                sui
+                                            </span>
+                                            <span
+                                                style={{
+                                                    display: 'flex',
+                                                    alignItems: 'center'
+                                                }}
+                                            >
+                                                {selectedUsers.some(user => user.username === 'sui') ? <BsCheckSquareFill /> : <FaRegSquare />}
                                             </span>
                                         </div>
                                     </li>
