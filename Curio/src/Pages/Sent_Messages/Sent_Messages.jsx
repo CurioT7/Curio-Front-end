f670b46946157420a879543cde8b27e1260a221b
omar-adel1 <omaradelhassan12@gmail.com>
2024-05-05 15:44:39 +0300

    "Add error state for empty sent messages and MessagesNavbar component in Sent\_Messages page"
    This commit adds a new error state for the case when there are no sent messages to display. It also includes the MessagesNavbar

--- Curio/src/Pages/Sent_Messages/Sent_Messages.jsx
+++ Curio/src/Pages/Sent_Messages/Sent_Messages.jsx
@@ -3,6 +3,7 @@ import "./Sent_Messages.css";
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 import Sent_Messages_Table from '../../Components/Sent_Messages_Table/Sent_Messages_Table';
+import MessagesNavbar from '../../Components/Messages/MessagesNavbar';
 
 const serverHost = import.meta.env.VITE_SERVER_HOST;
 
@@ -11,7 +12,7 @@ function Sent_Messages(props) {
     const [sentMessages, setSentMessages] = useState([]);
     const [details, setDetails] = useState([]);
     const username = localStorage.getItem('username');
-
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -35,6 +36,9 @@ function Sent_Messages(props) {
             const aboutresponse = await axios.get(`${serverHost}/api/user/${username}/about`);
             setSentMessages(response.data.messages);
             setDetails(aboutresponse.data);
+            if (response.data.messages.length === 0) {
+                setError(true);
+            }
         } catch (error) {
             if (error.response) {
                 if (error.response.status === 500) {
@@ -47,33 +51,44 @@ function Sent_Messages(props) {
     };
 
     return (
-        <div className='sent_message_content'>
-            <div 
-            style={{ 
-                marginTop: '70px', 
-                marginBottom:'70px',
-                width: '70%',
-                }}>
-                {sentMessages.map((message, index) => (
-                    <div key={message._id}>
-                        <div className='sent_message_table' 
-                            style={{
-                                background: index % 2 === 0 ? "#f6f7f8" : "white"
-                            }}>
-                            <Sent_Messages_Table
-                                subject={message.subject}
-                                message={message.message}
-                                timestamp={message.timestamp}
-                                sender={message.sender !== null ? message.sender.username: message.senderSubreddit.name}
-                                recipient={message.recipient !==null ? message.recipient.username: message.recipientSubreddit.name}
-                                isRecipientNull={!message.recipient}
-                                displayName={details.displayName}
-                                 />
-                        </div>
+        <div style={{ marginTop: "60px" }}>
+            <MessagesNavbar />
+            <div className='sent_message_content'>
+                {error ? (
+                    <div className="error-message">
+                        <p style={{
+                            padding: '20px',
+                        }}>there doesn't seem to be anything here
+                        </p>
                     </div>
-                ))}
+                ) : (
+                    <div
+                        style={{
+                            marginTop: '20px',
+                            marginBottom: '70px',
+                            width: '70%',
+                        }}>
+                        {sentMessages.map((message, index) => (
+                            <div key={message._id}>
+                                <div className='sent_message_table'
+                                    style={{
+                                        background: index % 2 === 0 ? "#f6f7f8" : "white"
+                                    }}>
+                                    <Sent_Messages_Table
+                                        subject={message.subject}
+                                        message={message.message}
+                                        timestamp={message.timestamp}
+                                        sender={message.sender !== null ? message.sender.username : message.senderSubreddit.name}
+                                        recipient={message.recipient !== null ? message.recipient.username : message.recipientSubreddit.name}
+                                        isRecipientNull={!message.recipient}
+                                        displayName={details.displayName}
+                                    />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
-
         </div>
     );
 }
