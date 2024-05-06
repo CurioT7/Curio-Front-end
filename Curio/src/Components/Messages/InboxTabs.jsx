c3663dad587b9d2df011628405d82ea0b291daa8
omar-adel1 <omaradelhassan12@gmail.com>
2024-05-06 13:24:43 +0300

    Update to MessagesInbox component and add UserName\_Mentions and Post\_Replies components
    - MessagesInbox component is removed and replaced with new components for InboxMessages, UserName\_Mentions, and Post

--- Curio/src/Components/Messages/InboxTabs.jsx
+++ Curio/src/Components/Messages/InboxTabs.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel, TabIndicator } from '@chakra-ui/react';
+import { useNavigate } from "react-router-dom"; 
 
 function InboxTabs() {
+    const navigate = useNavigate(); 
+
+    const handleTabClick = (path) => {
+        navigate(path); 
+    };
+
     return (
         <Tabs style={{backgroundColor : "#0079D3", color: "white", height: "2.5rem", paddingLeft: "80px"}} position='relative' variant='unstyled'>
         <TabList>
-            <Tab _selected={{borderBottom: "1px", borderColor: "white"}} _hover={{borderBottom: "1px", borderColor: "white"}} style={{fontSize: "0.8rem"}}>All</Tab>
-            <Tab _selected={{borderBottom: "1px", borderColor: "white"}} _hover={{borderBottom: "1px", borderColor: "white"}} style={{fontSize: "0.8rem"}}>Unread</Tab>
-            <Tab _selected={{borderBottom: "1px", borderColor: "white"}} _hover={{borderBottom: "1px", borderColor: "white"}} style={{fontSize: "0.8rem"}}>Messages</Tab>
-            <Tab _selected={{borderBottom: "1px", borderColor: "white"}} _hover={{borderBottom: "1px", borderColor: "white"}} style={{fontSize: "0.8rem"}}>Comment Replies</Tab>
-            <Tab _selected={{borderBottom: "1px", borderColor: "white"}} _hover={{borderBottom: "1px", borderColor: "white"}} style={{fontSize: "0.8rem"}}>Username Mentions</Tab>
+            <Tab _selected={{borderBottom: "1px", borderColor: "white"}} _hover={{borderBottom: "1px", borderColor: "white"}} style={{fontSize: "0.8rem"}} onClick={() => handleTabClick("")}>All</Tab>
+            <Tab _selected={{borderBottom: "1px", borderColor: "white"}} _hover={{borderBottom: "1px", borderColor: "white"}} style={{fontSize: "0.8rem"}} onClick={() => handleTabClick("")}>Unread</Tab>
+            <Tab _selected={{borderBottom: "1px", borderColor: "white"}} _hover={{borderBottom: "1px", borderColor: "white"}} style={{fontSize: "0.8rem"}} onClick={() => handleTabClick("")}>Messages</Tab>
+            <Tab _selected={{borderBottom: "1px", borderColor: "white"}} _hover={{borderBottom: "1px", borderColor: "white"}} style={{fontSize: "0.8rem"}} onClick={() => handleTabClick("/message/selfreply")}>Comment Replies</Tab>
+            <Tab _selected={{borderBottom: "1px", borderColor: "white"}} _hover={{borderBottom: "1px", borderColor: "white"}} style={{fontSize: "0.8rem"}} onClick={() => handleTabClick("/message/mentions")}>Username Mentions</Tab>
         </TabList>
          <TabIndicator mt='-1px' height='1px' bg='white.500' borderRadius='1px' />
         <TabPanels>
