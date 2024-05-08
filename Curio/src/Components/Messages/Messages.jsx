69eaa9a64b7566dac13837a9e3b2e94b706268b3
omar-adel1 <omaradelhassan12@gmail.com>
2024-05-09 00:36:22 +0300

    "Update Messages.jsx to include ReportPopup and BlockUserMessages; update Createpostarea.jsx to rename CSS file"
    This commit updates the Messages.jsx component to include a new ReportPopup component and the

--- Curio/src/Components/Messages/Messages.jsx
+++ Curio/src/Components/Messages/Messages.jsx
@@ -2,14 +2,37 @@ import React, { useEffect, useState } from "react";
 import "./Messages.css";
 import { fetchMessages } from "../../Pages/InboxMessages/InboxMessagesEndpoints";
 import axios from "axios";
+import ReportPopup from "./../ModalPages/ModalPages.jsx";
+import { BlockUserMessages } from '../../Pages/InboxMessages/InboxMessagesEndpoints.js';
 
 function Messages(props) {
+    const [showReportMenu, setShowReportMenu] = useState(false);
+    const [confirmBlock, setConfirmBlock] = useState(false);
+    const [recievedMessages, setRecievedMessages] = useState([]);
+    const token = localStorage.getItem('token');
 
+    const handleReportPopupClose = () => {
+        setShowReportMenu(false);
+    };
+
+    const handleBlockConfirmation = (sender, messageId) => {
+        setRecievedMessages(prevMessages => prevMessages.filter(message => message._id !== messageId));
+        BlockUserMessages(sender);
+    };
+
+    const handleReportClick = () => {
+        if (!token) {
+            navigate('/login');
+            setShowReportMenu(false);
+        }
+        else
+            setShowReportMenu(true);
+    };
 
     const handleDelete = async (id) => {
         try {
             const hostUrl = import.meta.env.VITE_SERVER_HOST;
-            const response = await axios.post(`${hostUrl}/api/message/delete/${id}`,{},{
+            const response = await axios.post(`${hostUrl}/api/message/delete/${id}`, {}, {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem('token')}`
                 }
@@ -25,7 +48,7 @@ function Messages(props) {
     const handleUnread = async (id) => {
         try {
             const hostUrl = import.meta.env.VITE_SERVER_HOST;
-            const response = await axios.post(`${hostUrl}/api/message/unread/${id}`,{},{
+            const response = await axios.post(`${hostUrl}/api/message/unread/${id}`, {}, {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem('token')}`
                 }
@@ -41,23 +64,35 @@ function Messages(props) {
     return (
         <div className="w-100 d-flex flex-column justify-content-center align-items-center">
             {props.messages.map((message, index) => (
-                <div key={index} className="d-flex flex-column col-md-8 p-3 mb-2" style={{ backgroundColor: "#F6F7F8" }}>
-                    <h5 className="message-title mb-3">hello:</h5>
-                    <div className="message-from ms-5">
-                        <div>
-                            <p className="secondary-message-text">from <a className="sender-style" href={`/user/${message.sender.username}`}>u/{message.sender.username}</a></p>
-                        </div>
-                        <div>
-                            <p>{message.message}</p>
-                        </div>
-                        <div className="d-flex">
-                            <p onClick={() => handleDelete(message._id)} className="private-messages-controls me-3">Delete</p>
-                            <p onClick={() => handleUnread(message._id)} className="private-messages-controls me-3">Mark unread</p>
-                            {/* <p className="private-messages-controls me-3">Report</p>
-                            <p className="private-messages-controls me-3">Block</p> */}
+                <>
+                    <div key={index} className="d-flex flex-column col-md-8 p-3 mb-2" style={{ backgroundColor: "#F6F7F8" }}>
+                        <h5 className="message-title mb-3">hello:</h5>
+                        <div className="message-from ms-5">
+                            <div>
+                                <p className="secondary-message-text">from <a className="sender-style" href={`/user/${message.sender?.username}`}>u/{message.sender?.username}</a></p>
+                            </div>
+                            <div>
+                                <p>{message.message}</p>
+                            </div>
+                            <div className="d-flex">
+                                <p onClick={() => handleDelete(message._id)} className="private-messages-controls me-3">Delete</p>
+                                <p onClick={() => handleUnread(message._id)} className="private-messages-controls me-3">Mark unread</p>
+                                <p className="private-messages-controls me-3" onClick={handleReportClick}>Report</p>
+                                {confirmBlock ? (
+                                    <>
+                                        <span className="private-messages-controls me-3" style={{ color: 'red' }}>are you sure?</span>
+                                        <p className="private-messages-controls me-3" as="li" onClick={() => handleBlockConfirmation(message.sender?.username, message._id)}>Yes</p>
+                                        <span className="private-messages-controls me-3">/</span>
+                                        <p className="private-messages-controls me-3" as="li" onClick={() => setConfirmBlock(false)}>No</p>
+                                    </>
+                                ) : (
+                                    <p className="private-messages-controls me-3" as="li" onClick={() => setConfirmBlock(true)}>Block User</p>
+                                )}
+                            </div>
                         </div>
                     </div>
-                </div>
+                    <ReportPopup show={showReportMenu} onHide={handleReportPopupClose} username={message.sender?.username} />
+                </>
             ))}
         </div>
     );
