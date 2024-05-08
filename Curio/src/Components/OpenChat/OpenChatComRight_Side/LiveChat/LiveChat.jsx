d88bdc969d65830fb172d4d063b8d633304a49de
Hassan <htarek1011@gmail.com>
2024-05-08 06:20:10 +0300

    refactor: Update server host URL in .env file and refactor Open_Chat_Page.js

--- Curio/src/Components/OpenChat/OpenChatComRight_Side/LiveChat/LiveChat.jsx
+++ Curio/src/Components/OpenChat/OpenChatComRight_Side/LiveChat/LiveChat.jsx
@@ -26,6 +26,7 @@ function LiveChat(props) {
   const [isPickerVisible, setPickerVisible] = useState(false);
   const [message, setMessage] = useState("");
   const [chatData, setChatData] = useState(null);
+  const user = localStorage.getItem("username");
   const [prevMessageDate, setPrevMessageDate] = useState(null);
   const username = localStorage.getItem("username");
 
@@ -44,35 +45,33 @@ function LiveChat(props) {
     fetchChatData();
   }, [props.chatId]);
 
-  // socket.on("newMessage", (message, username) => {
-  //     const recieverSocket = getRecieverSocket(username);
-  //     recieverSocket.emit("newMessage", message);
-  //   });
-
+  //recieve message
   useEffect(() => {
-    //for recieving messages
-    if (!socket) return;
-    socket.on("newMessage", (message, username) => {
-      console.log("new message", message);
-      console.log("username", username);
-      setChatData((prevData) => {
-        const chatIndex = prevData.chat.findIndex(
-          (chat) => chat._id === message.chatId
-        );
-        const chat = prevData.chat[chatIndex];
-        const newChat = {
-          ...chat,
-          messages: [...chat.messages, message],
-        };
-        const newChatData = [...prevData.chat];
-        newChatData[chatIndex] = newChat;
-        return {
-          ...prevData,
-          chat: newChatData,
-        };
-      });
+    if (!props.socket) return;
+    console.log("YAAAAAARAAb", props.socket);
+    props.socket.on("getMessage", (message, participants) => {
+      if (participants.includes(username)) {
+        setChatData((prev) => {
+          const newChatData = { ...prev };
+          const chatIndex = newChatData.chat.findIndex((chat) =>
+            chat.participants
+              .map((participant) => participant.username)
+              .includes(username)
+          );
+          newChatData.chat[chatIndex].messages.push({
+            message: message,
+            sender: username,
+            timestamp: new Date().toISOString(),
+          });
+          return newChatData;
+        });
+      }
     });
-  }, [socket]);
+
+    return () => {
+      props.socket.off("getMessage");
+    };
+  }, [props.socket, username]);
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
@@ -82,7 +81,22 @@ function LiveChat(props) {
     if (message.trim() !== "") {
       try {
         if (props.chatId) {
+          let participants = chatData.chat
+            .map((chat) =>
+              chat.participants.map((participant) => participant.username)
+            )
+            .flat();
+
           await sendMessageRequest(props.chatId, message, null);
+          console.log(props.socket);
+          if (props.socket) {
+            //TODO Change
+
+            console.log("RECIEVER", participants);
+
+            props.socket.emit("newMessage", message, participants);
+            console.log("message sent");
+          }
         } else {
           await createChatRequest(props.recipient, message);
         }
@@ -97,167 +111,205 @@ function LiveChat(props) {
     setMessage(message + emoji.native);
   };
 
-    return (
-        <div className='chat-div'>
-            <HeaderChatRight_Side header='General_Boat_962' check='true' />
-            <div style={{
-                overflow: 'auto',
-                width: '100%',
-                height: '100vh',
-                display: 'flex',
-                flexDirection: 'column'
-            }}>
-                <div className='Live-chat-form'>
-                    <div className='Live-chat-profile-data'>
-                        <a style={{
-                            textDecorationLine: 'none',
-                            color: '#2a3c42',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}>
-                            <div className='Live-chat-profile-picture'>
-                                <img src={profile} className="picture-live-chat" alt="" />
-                            </div>
-                            <div className='Live-chat-profile-username'>
-                                General_Boat_962
-                            </div>
-                            <div className='Live-chat-profile-username-details'>
-                                Redditor for 61d  ·  1 karma
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                {chatData && Array.isArray(chatData.chat) && chatData.chat.map((chat) => (
-                    <div key={chat._id} style={{ width: '100%' }}>
-                        {Array.isArray(chat.messages) && chat.messages.slice().reverse().map((message, index, array) => {
-                            const participant = chat.participants.find(participant => participant.id === message.sender);
-                            const profilePicture = participant ? participant.profilePicture || profile : profile;
-                            // Check if it's a new day or the first message
-                            const currentDate = formatDate(message.timestamp);
-                            const isFirstMessage = index === 0 || formatDate(array[index - 1].timestamp) !== currentDate;
-
-                            // Update previous date if it's a new day
-                            if (isFirstMessage && currentDate !== prevMessageDate) {
-                                setPrevMessageDate(currentDate);
-                            }
-
-                            // Render date only if it's a new day or the first message
-                            const renderDate = isFirstMessage || currentDate !== prevMessageDate;
-                            return (
-                                <div key={message._id}>
-                                    {renderDate && (
-                                        <div className='message-date-live-chat'>
-                                            <div className='date-line-beside' />
-                                            {prevMessageDate}
-                                            <div className='date-line-beside' />
-                                        </div>
-                                    )}
-                                    <div className='message-content-live-chat-container'>
-                                        <span className='image-chat-message'>
-                                            <img src={profilePicture} alt="" style={{ borderRadius: '20px' }} />
-                                        </span>
-                                        <div style={{
-                                            display: 'flex',
-                                            flexDirection: 'column',
-                                            gap: '0.5em'
-                                        }}>
-                                            <div style={{
-                                                display: 'flex',
-                                                flexDirection: 'row',
-                                                gap: '.25rem',
-                                                alignItems: 'center'
-                                            }}>
-                                                <span className='sender-name-live-chat'>
-                                                    {chat.senders.find(sender => sender.id === message.sender)?.username}
-                                                </span>
-                                                <span className='sender-time-live-chat'>{formatTimestamp(message.timestamp)}</span>
-                                            </div>
-                                            <span>{message.message}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                ))}
+  return (
+    <div className="chat-div">
+      <HeaderChatRight_Side header="General_Boat_962" check="true" />
+      <div className="Live-chat-form">
+        <div className="Live-chat-profile-data">
+          <a
+            style={{
+              textDecorationLine: "none",
+              color: "#2a3c42",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <div className="Live-chat-profile-picture">
+              <img src={profile} className="picture-live-chat" alt="" />
             </div>
-            <div className='chat-live-input'>
-                <form action="" className='form-input-live-chat'>
-                    <Button colorScheme='gray' variant='ghost'
-                        style={{
-                            paddingRight: '8px',
-                            paddingLeft: '8px',
-                            height: '40px',
-                            width: '40px',
-                            borderRadius: '100%'
-                        }}>
-                        <IoMdCamera style={{
-                            height: '40px',
-                            width: '40px',
-                        }} />
-                    </Button>
-                    <div className='live-chat-input-area'>
-                        <InputGroup size='md'>
-                            <Input
-                                placeholder="Message"
-                                size="md"
-                                style={{
-                                    background: '#eaedef',
-                                    border: '0px',
-                                    borderRadius: '20px',
-                                    fontSize: '1rem',
-                                    width: '100%'
-                                }}
-                                value={message}
-                                onChange={handleMessageChange}
-                            />
-                            <InputRightElement width='4.5rem'>
-                                <Tooltip hasArrow label='Select sticker' placement='top'>
-                                    <Button h='1.75rem' size='sm' colorScheme='teal' variant='ghost'
-                                        style={{
-                                            paddingRight: '8px',
-                                            paddingLeft: '8px',
-                                            height: '40px',
-                                            width: '40px',
-                                            borderRadius: '100%'
-                                        }}
-                                        onClick={() => setPickerVisible(!isPickerVisible)}>
-                                        <BsFillEmojiSmileFill style={{
-                                            height: '40px',
-                                            width: '40px',
-                                        }} />
-                                    </Button>
-                                </Tooltip>
-                            </InputRightElement>
-                        </InputGroup>
-                    </div>
-                    <Button colorScheme={message ? 'blue' : 'gray'} variant={message ? 'ghost' : 'none'}
-                        style={{
-                            paddingRight: '8px',
-                            paddingLeft: '8px',
-                            height: '40px',
-                            width: '40px',
-                            borderRadius: '100%',
-                            cursor: message ? 'pointer' : 'default'
-                        }}
-                        onClick={handleSend}>
-                        <IoMdSend style={{
-                            height: '40px',
-                            width: '40px',
-                        }} />
-                    </Button>
-                    {isPickerVisible && (
-                        <div ref={pickerRef} className="emoji-picker-container">
-                            <EmojiPicker data={data}
-                                previewPosition="bottom"
-                                onEmojiSelect={handleEmojiSelect} />
-                        </div>
-                    )}
-                </form>
+            <div className="Live-chat-profile-username">General_Boat_962</div>
+            <div className="Live-chat-profile-username-details">
+              Redditor for 61d · 1 karma
             </div>
+          </a>
         </div>
-    );
+      </div>
+      {chatData &&
+        Array.isArray(chatData.chat) &&
+        chatData.chat.map((chat) => (
+          <div key={chat._id} style={{ width: "100%" }}>
+            {Array.isArray(chat.messages) &&
+              chat.messages
+                .slice()
+                .reverse()
+                .map((message, index, array) => {
+                  const participant = chat.participants.find(
+                    (participant) => participant.id === message.sender
+                  );
+                  const profilePicture = participant
+                    ? participant.profilePicture || profile
+                    : profile;
+                  // Check if it's a new day or the first message
+                  const currentDate = formatDate(message.timestamp);
+                  const isFirstMessage =
+                    index === 0 ||
+                    formatDate(array[index - 1].timestamp) !== currentDate;
+
+                  // Update previous date if it's a new day
+                  if (isFirstMessage && currentDate !== prevMessageDate) {
+                    setPrevMessageDate(currentDate);
+                  }
+
+                  // Render date only if it's a new day or the first message
+                  const renderDate =
+                    isFirstMessage || currentDate !== prevMessageDate;
+                  return (
+                    <div key={message._id}>
+                      {renderDate && (
+                        <div className="message-date-live-chat">
+                          <div className="date-line-beside" />
+                          {prevMessageDate}
+                          <div className="date-line-beside" />
+                        </div>
+                      )}
+                      <div className="message-content-live-chat-container">
+                        <span className="image-chat-message">
+                          <img
+                            src={profilePicture}
+                            alt=""
+                            style={{ borderRadius: "20px" }}
+                          />
+                        </span>
+                        <div
+                          style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: "0.5em",
+                          }}
+                        >
+                          <div
+                            style={{
+                              display: "flex",
+                              flexDirection: "row",
+                              gap: ".25rem",
+                              alignItems: "center",
+                            }}
+                          >
+                            <span className="sender-name-live-chat">
+                              {
+                                chat.senders.find(
+                                  (sender) => sender.id === message.sender
+                                )?.username
+                              }
+                            </span>
+                            <span className="sender-time-live-chat">
+                              {formatTimestamp(message.timestamp)}
+                            </span>
+                          </div>
+                          <span>{message.message}</span>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+          </div>
+        ))}
+      <div className="chat-live-input">
+        <form action="" className="form-input-live-chat">
+          <Button
+            colorScheme="gray"
+            variant="ghost"
+            style={{
+              paddingRight: "8px",
+              paddingLeft: "8px",
+              height: "40px",
+              width: "40px",
+              borderRadius: "100%",
+            }}
+          >
+            <IoMdCamera
+              style={{
+                height: "40px",
+                width: "40px",
+              }}
+            />
+          </Button>
+          <div className="live-chat-input-area">
+            <InputGroup size="md">
+              <Input
+                placeholder="Message"
+                size="md"
+                style={{
+                  background: "#eaedef",
+                  border: "0px",
+                  borderRadius: "20px",
+                  fontSize: "1rem",
+                  width: "100%",
+                }}
+                value={message}
+                onChange={handleMessageChange}
+              />
+              <InputRightElement width="4.5rem">
+                <Tooltip hasArrow label="Select sticker" placement="top">
+                  <Button
+                    h="1.75rem"
+                    size="sm"
+                    colorScheme="teal"
+                    variant="ghost"
+                    style={{
+                      paddingRight: "8px",
+                      paddingLeft: "8px",
+                      height: "40px",
+                      width: "40px",
+                      borderRadius: "100%",
+                    }}
+                    onClick={() => setPickerVisible(!isPickerVisible)}
+                  >
+                    <BsFillEmojiSmileFill
+                      style={{
+                        height: "40px",
+                        width: "40px",
+                      }}
+                    />
+                  </Button>
+                </Tooltip>
+              </InputRightElement>
+            </InputGroup>
+          </div>
+          <Button
+            colorScheme={message ? "blue" : "gray"}
+            variant={message ? "ghost" : "none"}
+            style={{
+              paddingRight: "8px",
+              paddingLeft: "8px",
+              height: "40px",
+              width: "40px",
+              borderRadius: "100%",
+              cursor: message ? "pointer" : "default",
+            }}
+            onClick={handleSend}
+          >
+            <IoMdSend
+              style={{
+                height: "40px",
+                width: "40px",
+              }}
+            />
+          </Button>
+          {isPickerVisible && (
+            <div ref={pickerRef} className="emoji-picker-container">
+              <EmojiPicker
+                data={data}
+                previewPosition="bottom"
+                onEmojiSelect={handleEmojiSelect}
+              />
+            </div>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default LiveChat;
