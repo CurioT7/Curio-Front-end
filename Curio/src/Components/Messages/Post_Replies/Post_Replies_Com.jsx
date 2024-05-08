67750830ca1dda2441311c115cff03e24898dcd4
omar-adel1 <omaradelhassan12@gmail.com>
2024-05-08 05:16:05 +0300

    "Update Post\_Replies\_Com.jsx to Post\_Replies\_Com, update UserName\_Mentions\_Com.css for responsiveness, and fix console.log in NewChat.jsx"
    This commit

--- Curio/src/Components/Messages/Post_Replies/Post_Replies_Com.jsx
+++ Curio/src/Components/Messages/Post_Replies/Post_Replies_Com.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Flex, Box } from "@chakra-ui/react";
 import Upvotes from '../../../styles/icons/Upvotes.jsx';
 import Downvotes from '../../../styles/icons/Downvotes.jsx';
@@ -8,20 +8,21 @@ import { getTimeDifference } from '../../getTimeDifference/getTimeDifference';
 import { handleUPVoteComments, handleDownVoteComments, BlockUserMessages } from '../../../Pages/InboxMessages/InboxMessagesEndpoints.js';
 import { Link } from 'react-router-dom';
 
-function UserName_Mentions_Com(props) {
-    const [upvoted, setUpvoted] = useState(false); 
-    const [downvoted, setDownvoted] = useState(false); 
+function Post_Replies_Com(props) {
+    const [upvoted, setUpvoted] = useState(false);
+    const [downvoted, setDownvoted] = useState(false);
+    const [confirmBlock, setConfirmBlock] = useState(false);
 
     useEffect(() => {
         const isDownvoted = props.downvotedcomments.some(comment => comment._id === props.itemId);
         const isUpvoted = props.upvotedcomments.some(comment => comment._id === props.itemId);
-        
+
         setDownvoted(isDownvoted);
         setUpvoted(isUpvoted);
     }, [props.downvotedcomments, props.upvotedcomments, props.itemId]);
 
     return (
-        <div style={{width: "65%"}} className='username-mentions-table d-flex justify-content-center'>
+        <div style={{ width: "65%" }} className='username-mentions-table d-flex justify-content-center'>
             <h5 className='username-mentions-header-messages'>post reply:
                 <a href="" className='username-mentions-title-messages'>
                     {props.title}
@@ -38,10 +39,10 @@ function UserName_Mentions_Com(props) {
                     flexDirection: 'column'
                 }}>
                     <button className='me-2 upvotes-footer-button' onClick={() => handleUPVoteComments(props.itemId, upvoted, setUpvoted, setDownvoted)}>
-                        {upvoted ? <FilledUpvote colorFill="#D93A00"/> : <Upvotes />}
+                        {upvoted ? <FilledUpvote colorFill="#D93A00" /> : <Upvotes />}
                     </button>
                     <button className='downvotes-footer-button' onClick={() => handleDownVoteComments(props.itemId, downvoted, setUpvoted, setDownvoted)}>
-                        {downvoted ? <FilledDownvote colorFill="#6A5CFF"/> : <Downvotes />}
+                        {downvoted ? <FilledDownvote colorFill="#6A5CFF" /> : <Downvotes />}
                     </button>
                 </div>
                 <div className='username-mention-details'>
@@ -51,7 +52,7 @@ function UserName_Mentions_Com(props) {
                                 color: '#0079d3',
                                 marginRight: '0.5rem'
                             }}>/u/{props.sender}</Link>
-                            {props.linkedSubreddit && ( 
+                            {props.linkedSubreddit && (
                                 <>
                                     via <Link to={`/r/${props.linkedSubreddit}`} style={{
                                         color: '#0079d3',
@@ -66,11 +67,20 @@ function UserName_Mentions_Com(props) {
                     }}>
                         <a href="">{props.message}</a>
                     </div>
-                    <Box as="ul" listStyleType="none" display="flex" gap='0.5rem' justifyContent='flex-start' paddingLeft='0'>
+                    <Box as="ul" listStyleType="none" display="flex" gap='0.5rem' justifyContent='flex-start' paddingLeft='0' flexWrap='wrap' width='100%'>
                         <Box className="list-item-username-mentions" as="li" paddingLeft='0'>Context</Box>
                         <Box className="list-item-username-mentions" as="li">Full Comments({props.noComments})</Box>
                         <Box className="list-item-username-mentions" as="li">Report</Box>
-                        <Box className="list-item-username-mentions" as="li" onClick={() => BlockUserMessages(props.sender)}>Block User</Box>
+                        {confirmBlock ? (
+                            <>
+                                <span className="list-item-username-mentions check-block-message" style={{ color: 'red' }}>are you sure?</span>
+                                <Box className="list-item-username-mentions check-block-message" as="li" onClick={() => BlockUserMessages(props.sender)}>Yes</Box>
+                                <span className="list-item-username-mentions">/</span>
+                                <Box className="list-item-username-mentions check-block-message" as="li" onClick={() => setConfirmBlock(false)}>No</Box>
+                            </>
+                        ) : (
+                            <Box className="list-item-username-mentions" as="li" onClick={() => setConfirmBlock(true)}>Block User</Box>
+                        )}
                         <Box className="list-item-username-mentions" as="li">Mark Unread</Box>
                     </Box>
                 </div>
@@ -79,4 +89,4 @@ function UserName_Mentions_Com(props) {
     );
 }
 
-export default UserName_Mentions_Com;
+export default Post_Replies_Com;
