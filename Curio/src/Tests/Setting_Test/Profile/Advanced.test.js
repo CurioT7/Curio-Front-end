dcb874e317b0c0b80a088c3e9b6f1569063f613c
omar-adel1 <omaradelhassan12@gmail.com>
2024-05-10 00:01:16 +0300

    Update @testing-library/jest-dom to version 6.4.5
    * package-lock.json: changed "@testing-library/jest-dom" version from 6.4.2 to 6.

---
+++ Curio/src/Tests/Setting_Test/Profile/Advanced.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Advanced from '../../../Components/profileSetting/Advanced/Advanced';
+
+// Add the necessary matchers
+const { toBeInTheDocument, toHaveAttribute } = require('@testing-library/jest-dom/matchers');
+expect.extend({ toBeInTheDocument, toHaveAttribute });
+
+// Mocking the sendUserDataToBackend function
+jest.mock('../../../Components/UserSetting/UserSettingsEndPoints.js', () => ({
+  sendUserDataToBackend: jest.fn(),
+}));
+
+describe('Advanced component', () => {
+  it('renders correctly', () => {
+    // Mock userData prop
+    const userData = {
+      allowFollow: true,
+      contentVisibility: true,
+      activeInCommunityVisibility: true,
+      clearHistory: false,
+    };
+
+    const { getByText, getByTestId } = render(<Advanced userData={userData} />);
+
+    // Check if important elements are rendered
+    expect(getByText('Allow people to follow you')).toBeInTheDocument();
+    expect(getByTestId('follow-switch')).toBeInTheDocument();
+    expect(getByText('Content visibility')).toBeInTheDocument();
+    expect(getByTestId('content-visibility-switch')).toBeInTheDocument();
+    expect(getByText('Active in communities visibility')).toBeInTheDocument();
+    expect(getByTestId('communities-visibility-switch')).toBeInTheDocument();
+    expect(getByText(/Clear history/i)).toBeInTheDocument();
+    expect(getByTestId('Clear-history')).toBeInTheDocument();
+  });
+
+  it('toggles follow switch correctly', () => {
+    const userData = {
+      allowFollow: true,
+      contentVisibility: true,
+      activeInCommunityVisibility: true,
+      clearHistory: false,
+    };
+
+    const { getByTestId } = render(<Advanced userData={userData} />);
+
+    // Simulate a click on the follow switch
+    fireEvent.click(getByTestId('follow-switch'));
+
+    // Check if the follow switch state is toggled
+    expect(getByTestId('follow-switch')).toHaveAttribute('aria-checked', 'false');
+
+    // Check if sendUserDataToBackend is called with the correct argument
+    expect(sendUserDataToBackend).toHaveBeenCalledWith({ allowFollow: false });
+  });
+
+  // Write similar tests for toggling content visibility, communities visibility, and clearing history
+});
