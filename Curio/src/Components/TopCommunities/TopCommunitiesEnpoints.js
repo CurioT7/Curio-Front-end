e9b9ade002d2f8064f362c9c436d9023bc26911d
Ahmed Okasha <95878292+hmdOkasha@users.noreply.github.com>
2024-05-10 16:22:46 +0300

    functional

--- Curio/src/Components/TopCommunities/TopCommunitiesEnpoints.js
+++ Curio/src/Components/TopCommunities/TopCommunitiesEnpoints.js
@@ -4,6 +4,11 @@ import axios from "axios";
 const hostUrl = import.meta.env.VITE_SERVER_HOST;
 
 
+/**
+ * Fetches community information from the server.
+ * @param {number} page - The page number to fetch.
+ * @module TopCommunities
+ */
 export async function showCommunityInformation(page) {
   try {
     const response = await axios.get(`${hostUrl}/api/best/communities?page=${page}`);
