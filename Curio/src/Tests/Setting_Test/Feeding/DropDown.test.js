b297cbd13eb075bdadfb580e4fbd87445d3d2b27
Ali-9-25 <alykhazback@gmail.com>
2024-05-11 23:51:05 +0300

    Reverting error

--- Curio/src/Tests/Setting_Test/Feeding/DropDown.test.js
+++ Curio/src/Tests/Setting_Test/Feeding/DropDown.test.js
@@ -1,25 +1,25 @@
-// import DropDown from "../../../Components/feedSettings/childs/DropDown";
-// import React from 'react';
-// import { render, fireEvent } from '@testing-library/react';
-// import '@testing-library/jest-dom';
+import DropDown from "../../../Components/feedSettings/childs/DropDown";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 
-// describe('DropDown', () => {
-//     it('renders without crashing', () => {
-//       render(<DropDown />);
-//     });
-  
-//     it('calls onChangeSort when a sort option is selected', () => {
-//       const handleChangeSort = jest.fn();
-//       const { getByTestId } = render(<DropDown onChangeSort={handleChangeSort} isSort />);
-//       fireEvent.change(getByTestId('sort-dropdown'), { target: { value: 'New' } });
-//       expect(handleChangeSort).toHaveBeenCalled();
-//     });
-  
-//     it('calls onChangeRemember when the remember switch is clicked', () => {
-//       const handleChangeRemember = jest.fn();
-//       const { getByTestId } = render(<DropDown onChangeRemember={handleChangeRemember} />);
-//       fireEvent.click(getByTestId('remember-switch'));
-//       expect(handleChangeRemember).toHaveBeenCalled();
-//     });
-//   });
+describe('DropDown', () => {
+  it('renders without crashing', () => {
+    render(<DropDown />);
+  });
+
+  it('calls onChangeSort when a sort option is selected', () => {
+    const handleChangeSort = jest.fn();
+    const { getByTestId } = render(<DropDown onChangeSort={handleChangeSort} isSort />);
+    fireEvent.change(getByTestId('sort-dropdown'), { target: { value: 'New' } });
+    expect(handleChangeSort).toHaveBeenCalled();
+  });
+
+  it('calls onChangeRemember when the remember switch is clicked', () => {
+    const handleChangeRemember = jest.fn();
+    const { getByTestId } = render(<DropDown onChangeRemember={handleChangeRemember} />);
+    fireEvent.click(getByTestId('remember-switch'));
+    expect(handleChangeRemember).toHaveBeenCalled();
+  });
+});
